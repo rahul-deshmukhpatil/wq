e5e3cb3122574e8da5d8569865baa19e1f4794a8
Rahul Patil <rahulpatil@bmlltech.com>
2021-09-24 18:18:11 +0100

    average

--- econometrics/group-work-1/process_graph_step-1-4.py
+++ econometrics/group-work-1/process_graph_step-1-4.py
@@ -1,5 +1,3 @@
-import requests
-import csv
 import yfinance as yf
 import pandas as pd
 
@@ -16,32 +14,54 @@ SPDR_GOLD_ETF = 'GLD'
 VOODOO_FTSE_ETF = 'VGK'
 
 # 10 Years treasury data, so filter using START_DATE and END_DATE
-treasury_csv_data = pd.read_csv(NASDAQ_TREASURY_DATA_CSV_URL, header=0, parse_dates=[
-    0], infer_datetime_format=True).loc[START_DATE:END_DATE]
+treasury_yield_df_10yr = pd.read_csv(NASDAQ_TREASURY_DATA_CSV_URL, header=0, parse_dates=[0],
+                                infer_datetime_format=True)
+treasury_yield_df = treasury_yield_df_10yr.loc[START_DATE:END_DATE]
+treasury_yield_df = treasury_yield_df_10yr.loc[(treasury_yield_df_10yr.Date >= START_DATE) & (treasury_yield_df_10yr.Date <=END_DATE)]
+treasury_yield_df.set_index('Date')
 
 # Yahoo returns last day of Sept as well, so filter using START_DATE and END_DATE
 gold_etf_df = yf.download(
     [SPDR_GOLD_ETF], start=START_DATE, end=END_DATE).loc[START_DATE:END_DATE]
 ftse_etf_df = yf.download([VOODOO_FTSE_ETF], start=START_DATE, end=END_DATE).loc[
-    START_DATE:END_DATE]
+              START_DATE:END_DATE]
 
-# calculate daily returns of gold etf
+# 2.1	Compute the daily returns of your Gold ETF
 gold_etf_df['Daily_returns'] = gold_etf_df['Open'] - gold_etf_df['Close']
+# 2.1	Compute the daily returns of your Gold ETF
 ftse_etf_df['Daily_returns'] = ftse_etf_df['Open'] - ftse_etf_df['Close']
 
 
 def calculate_monthly_avg(df, col_name, month):
     monthly_avg = df[col_name].groupby(
-        gold_etf_df.index.map(lambda x: x.month)).mean()
+        df.index.map(lambda x: x.month)).mean()
     return monthly_avg[month]
 
 
-# avg gold returns for the month Oct and Nov 2019
-monthly_gold_etf_avg = gold_etf_df['Close'].groupby(
+monthly_avg = gold_etf_df['Close'].groupby(
     gold_etf_df.index.map(lambda x: x.month)).mean()
+calculate_monthly_avg(gold_etf_df, 'Close', 10)
 
-# avg FTSE returns for the month Oct and Nov 2019
-monthly_ftse_etf_avg = ftse_etf_df['Close'].groupby(
-    ftse_etf_df.index.map(lambda x: x.month)).mean()
+# 3.1 to 3.3
+# calculate avg yield/price of bonds and ETFs for Oct19 and Nov19
+calculate_monthly_avg(treasury_yield_df, '2 YR', 10)
+calculate_monthly_avg(treasury_yield_df, '3 YR', 10)
+calculate_monthly_avg(treasury_yield_df, '5 YR', 10)
+calculate_monthly_avg(treasury_yield_df, '7 YR', 10)
+calculate_monthly_avg(treasury_yield_df, '10 YR', 10)
+calculate_monthly_avg(treasury_yield_df, '30 YR', 10)
+calculate_monthly_avg(gold_etf_df, 'Close', 10)
+calculate_monthly_avg(ftse_etf_df, 'Close', 10)
+
+calculate_monthly_avg(treasury_yield_df, '2 YR', 11)
+calculate_monthly_avg(treasury_yield_df, '3 YR', 11)
+calculate_monthly_avg(treasury_yield_df, '5 YR', 11)
+calculate_monthly_avg(treasury_yield_df, '7 YR', 11)
+calculate_monthly_avg(treasury_yield_df, '10 YR', 11)
+calculate_monthly_avg(treasury_yield_df, '30 YR', 11)
+calculate_monthly_avg(gold_etf_df, 'Close', 11)
+calculate_monthly_avg(ftse_etf_df, 'Close', 11)
+
+
 
 pass
