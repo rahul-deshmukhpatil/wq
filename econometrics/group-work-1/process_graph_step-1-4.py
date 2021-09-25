708cc3b0519e2bf9c21a293a1046bfaf57dbc947
rahul patil <rahul.deshmukhpatil@gmail.com>
2021-09-25 14:33:55 +0100

    html output

--- econometrics/group-work-1/process_graph_step-1-4.py
+++ econometrics/group-work-1/process_graph_step-1-4.py
@@ -1,9 +1,10 @@
 import yfinance as yf
 import pandas as pd
-from matplotlib import pyplot as plt
 
 START_DATE = '2019-10-01'
 END_DATE = '2019-11-30'
+OCT_MONTH = 10
+NOV_MONTH = 11
 NASDAQ_TREASURY_DATA_CSV_URL = \
     "https://data.nasdaq.com/api/v3/datasets/USTREASURY/YIELD.csv?api_key=x6ZcYtB4RzvAVop7E-U6"
 
@@ -18,12 +19,11 @@ VOODOO_FTSE_ETF = 'VGK'
 # 10 Years treasury data, so filter using START_DATE and END_DATE
 treasury_yield_df = pd.read_csv(NASDAQ_TREASURY_DATA_CSV_URL, header=0, parse_dates=[0],
                                 infer_datetime_format=True) \
-                        .set_index('Date') \
-                       # .loc[START_DATE:END_DATE]
-
+    .set_index('Date')
 labels_to_drop = ['1 MO', '2 MO', '3 MO', '6 MO', '1 YR', '20 YR']
 treasury_yield_df = treasury_yield_df.drop(columns=labels_to_drop)
-treasury_yield_df = treasury_yield_df .loc[(treasury_yield_df.index >= START_DATE) & (treasury_yield_df.index <= END_DATE)]
+treasury_yield_df = treasury_yield_df.loc[
+    (treasury_yield_df.index >= START_DATE) & (treasury_yield_df.index <= END_DATE)]
 
 # Yahoo returns last day of Sept as well, so filter using START_DATE and END_DATE
 gold_etf_df = yf.download(
@@ -43,53 +43,80 @@ def calculate_monthly_avg_price_or_yield(df, col_name, month):
     return monthly_average[month]
 
 
-# 3.1 to 3.3
-# calculate avg yield/price of bonds and ETFs for Oct19 and Nov19
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '2 YR', 10)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '3 YR', 10)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '5 YR', 10)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '7 YR', 10)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '10 YR', 10)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '30 YR', 10)
-calculate_monthly_avg_price_or_yield(gold_etf_df, 'Close', 10)
-calculate_monthly_avg_price_or_yield(ftse_etf_df, 'Close', 10)
-
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '2 YR', 11)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '3 YR', 11)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '5 YR', 11)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '7 YR', 11)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '10 YR', 11)
-calculate_monthly_avg_price_or_yield(treasury_yield_df, '30 YR', 11)
-calculate_monthly_avg_price_or_yield(gold_etf_df, 'Close', 11)
-calculate_monthly_avg_price_or_yield(ftse_etf_df, 'Close', 11)
-
-
 def calculate_std_deviation_in_price_or_yield(df, col_name, month):
     monthly_average = df[col_name].groupby(
         df.index.map(lambda x: x.month)).std()
     return monthly_average[month]
 
 
+# 3.1 to 3.3
+# calculate avg yield/price of bonds and ETFs for Oct19 and Nov19
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '2 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '3 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '5 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '7 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '10 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '30 YR', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(gold_etf_df, 'Close', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(ftse_etf_df, 'Close', OCT_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '2 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '3 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '5 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '7 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '10 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(treasury_yield_df, '30 YR', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(gold_etf_df, 'Close', NOV_MONTH)
+
+calculate_monthly_avg_price_or_yield(ftse_etf_df, 'Close', NOV_MONTH)
+
 # 3.4 to 3.6
 # calculate  std deviation in yield/price of bonds and ETFs for Oct19 and Nov19
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '2 YR', 10)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '3 YR', 10)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '5 YR', 10)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '7 YR', 10)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '10 YR', 10)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '30 YR', 10)
-calculate_std_deviation_in_price_or_yield(gold_etf_df, 'Close', 10)
-calculate_std_deviation_in_price_or_yield(ftse_etf_df, 'Close', 10)
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '2 YR', OCT_MONTH)
 
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '2 YR', 11)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '3 YR', 11)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '5 YR', 11)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '7 YR', 11)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '10 YR', 11)
-calculate_std_deviation_in_price_or_yield(treasury_yield_df, '30 YR', 11)
-calculate_std_deviation_in_price_or_yield(gold_etf_df, 'Close', 11)
-calculate_std_deviation_in_price_or_yield(ftse_etf_df, 'Close', 11)
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '3 YR', OCT_MONTH)
 
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '5 YR', OCT_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '7 YR', OCT_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '10 YR', OCT_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '30 YR', OCT_MONTH)
+
+calculate_std_deviation_in_price_or_yield(gold_etf_df, 'Close', OCT_MONTH)
+
+calculate_std_deviation_in_price_or_yield(ftse_etf_df, 'Close', OCT_MONTH)
+
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '2 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '3 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '5 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '7 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '10 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(treasury_yield_df, '30 YR', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(gold_etf_df, 'Close', NOV_MONTH)
+
+calculate_std_deviation_in_price_or_yield(ftse_etf_df, 'Close', NOV_MONTH)
 
 # 4.1 Graph the 6 benchmark securities for the 2 months on 1 plot
 treasury_yield_df.plot(ylabel='Yields', grid=True)
@@ -99,6 +126,7 @@ treasury_yield_df.plot(ylabel='Yields', grid=True)
 etf_prices = ftse_etf_df
 etf_prices['fste_etf_price'] = ftse_etf_df['Close']
 etf_prices['gold_etf_price'] = gold_etf_df['Close']
-etf_prices.plot(y=['fste_etf_price', 'gold_etf_price'], ylabel='gold_etf_price', secondary_y=['fste_etf_price'], grid=True)
+etf_prices.plot(y=['fste_etf_price', 'gold_etf_price'], ylabel='gold_etf_price', secondary_y=['fste_etf_price'],
+                grid=True)
 
 pass
