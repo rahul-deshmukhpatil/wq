c15adcc60a104a85c030f10f3914666e5c89d6fd
rahul patil <rahul.deshmukhpatil@gmail.com>
2021-09-30 13:31:43 +0100

    corrected create env script

--- econometrics/group-work-2/create_venv.sh
+++ econometrics/group-work-2/create_venv.sh
@@ -3,6 +3,6 @@ VENV_DIR="$HOME/venv/$VENV_NAME"
 
 echo "$VENV_NAME : $VENV_DIR"
 #rm -rf $VENV_DIR 
-python3 -m venv "$VENV_DIR"
-source "$VENV_DIR/bin/activate"
+python3 -m venv $VENV_DIR
+source $VENV_DIR/bin/activate
 pip install -r requirements.txt
